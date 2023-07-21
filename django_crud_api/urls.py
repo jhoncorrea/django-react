386104896680052ccfccc0ae32f0d0187ac9001e
jhon <jhon.elias.correa2013@gmail.com>
2023-07-21 18:02:25 -0500

    36

--- django_crud_api/urls.py
+++ django_crud_api/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from django.conf.urls.static import static
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
-#from django.conf import settings
+from django.conf import settings
 
 
 urlpatterns = [
@@ -27,4 +27,4 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
 
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
